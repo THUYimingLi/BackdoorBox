c93426253f763a2b644407d5dc6fc2e3cf96f54f
yangsheng <1547010935@qq.com>
2022-05-09 11:59:46 +0800

    modify finetuning

--- core/defenses/FineTuning.py
+++ core/defenses/FineTuning.py
@@ -1,5 +1,6 @@
 '''
-This is the implement of finetuning.
+This is the implement of fine-tuning proposed in [1].
+[1] Fine-Pruning: Defending Against Backdooring Attacks on Deep Neural Networks. RAID, 2018.
 '''
 
 
@@ -32,7 +33,6 @@ def adjust_learning_rate(lr, optimizer, epoch):
 
 class FineTuning(Base):
     """FineTuning process.
-
     Args:
         train_dataset (types in support_list): Benign training dataset.
         test_dataset (types in support_list): Benign testing dataset.
@@ -68,7 +68,6 @@ class FineTuning(Base):
 
     def frozen(self):
         """Frozen the layers which don't need to fine tuning.
-
         """
         if self.layer==None or self.layer[0]=="full layers":
             return
@@ -78,9 +77,8 @@ class FineTuning(Base):
                     for param in child.parameters():
                         param.requires_grad = False
 
-    def finetuning(self,schedule=None):
+    def repair(self,schedule=None):
         """Finetuning.
-
         Args:
             schedule (dict): Schedule for testing.
         """
@@ -104,7 +102,7 @@ class FineTuning(Base):
                 device = torch.device("cuda:0")
             else:
                 gpus = list(range(current_schedule['GPU_num']))
-                model = nn.DataParallel(self.model.cuda(), device_ids=gpus, output_device=gpus[0])
+                self.model = nn.DataParallel(self.model.cuda(), device_ids=gpus, output_device=gpus[0])
                 # TODO: DDP training
                 pass
         # Use CPU
@@ -160,7 +158,6 @@ class FineTuning(Base):
 
     def test(self,schedule=None):
         """Test the finetuning model.
-
         Args:
             schedule (dict): Schedule for testing.
         """
@@ -170,8 +167,5 @@ class FineTuning(Base):
             raise AttributeError("Test set is None, please check your setting.")
         test(self.model,self.test_dataset,schedule)
 
-
-
-
-
-
+    def get_model(self):
+        return self.model
