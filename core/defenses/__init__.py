23c9b9c75202554d287ca4b6cba8dfea7c99315d
ProbeTS <1227643856@qq.com>
2022-07-09 16:26:16 +0800

    add CutMix init

--- core/defenses/__init__.py
+++ core/defenses/__init__.py
@@ -5,7 +5,8 @@ from .MCR import MCR
 from .FineTuning import FineTuning
 from .NAD import NAD
 from .Pruning import Pruning
+from .CutMix import CutMix
 
 __all__ = [
-    'AutoEncoderDefense', 'ShrinkPad', 'FineTuning', 'MCR', 'NAD', 'Pruning', 'ABL'
+    'AutoEncoderDefense', 'ShrinkPad', 'FineTuning', 'MCR', 'NAD', 'Pruning', 'ABL', 'CutMix',
 ]
