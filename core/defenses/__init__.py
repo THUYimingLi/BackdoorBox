bec5dfbc2e1a3feb98e4ae07bfe6e46e2dd9cc60
ProbeTS <1227643856@qq.com>
2022-07-09 16:25:19 +0800

    add

--- core/defenses/__init__.py
+++ core/defenses/__init__.py
@@ -4,8 +4,7 @@ from .FineTuning import FineTuning
 from .NAD import NAD
 from .Pruning import Pruning
 from .ABL import ABL
-from .CutMix import CutMix
 
 __all__ = [
-    'ShrinkPad', 'FineTuning', 'MCR', 'NAD', 'Pruning', 'ABL', 'CutMix',
+    'ShrinkPad', 'FineTuning', 'MCR', 'NAD', 'Pruning', 'ABL'
 ]
